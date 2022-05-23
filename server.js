603de83bc619498c3625fd1b16a9d84b38bd6dd2
Fractal393 <akshatdalmiait2@vjit.ac.in>
2022-05-23 22:03:25 +0530

    change url for backend

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://shrouded-earth-76176.herokuapp.com:3000",
   credentials: true,
 }));
 
